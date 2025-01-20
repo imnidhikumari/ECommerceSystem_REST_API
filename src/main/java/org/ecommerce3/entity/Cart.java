49733951a14c530e8e54a291506abd9bc5d2c886
imnidhikumari <nidshiv55@gmail.com>
2025-01-20 14:20:32 +0530

    entity classes added

--- src/main/java/org/ecommerce3/entity/Cart.java
+++ src/main/java/org/ecommerce3/entity/Cart.java
@@ -1,4 +1,30 @@
 package org.ecommerce3.entity;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name="user")
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name="cartId", nullable = false)
+    @NotNull
+    Double cartId;
+
+    @ManyToOne(fetch = FetchType.LAZY) // As many carts can belong to one user
+    @JoinColumn(name = "userId", nullable = false)
+    User user;
+
 }
--- src/main/java/org/ecommerce3/entity/User.java
+++ src/main/java/org/ecommerce3/entity/User.java
@@ -1,5 +1,42 @@
 package org.ecommerce3.entity;
 
-public class User {
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name="user")
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name="userId", nullable = false)
+    @NotNull
+    Double userId;
+
+    @Column(name="name", nullable = false)
+    @NotBlank
+    String name;
+
+    @Column(name="email", nullable = false)
+    @NotBlank
+    String email;
+
+    @Column(name="password", unique=true, nullable = false)
+    @NotBlank
+    String password;
+
+    @Column(name="createdAt", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    Date createdAt;
 }

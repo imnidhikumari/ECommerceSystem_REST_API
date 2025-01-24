95fef58659306c93c8dec6c29f469c20e215d4b9
imnidhikumari <nidshiv55@gmail.com>
2025-01-24 22:56:16 +0530

    cart Module added

--- src/main/java/org/ecommerce3/repository/CartRepository.java
+++ src/main/java/org/ecommerce3/repository/CartRepository.java
@@ -1,8 +1,12 @@
 package org.ecommerce3.repository;
 
 import org.ecommerce3.entity.Cart;
+import org.ecommerce3.entity.Users;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
 
 public class CartRepository {
     private final SessionFactory sessionFactory;
@@ -17,6 +21,14 @@ public class CartRepository {
             session.saveOrUpdate(cart);
             session.getTransaction().commit();
             return cart;
+        }
+    }
+
+    public Optional<Cart> getAllCartByUserId(String userId) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Users> query = session.createNamedQuery("getAllCartByUserId", Cart.class);
+            query.setParameter("userId", userId);
+            return Optional.ofNullable(query.uniqueResult());
         }
     }
 }
